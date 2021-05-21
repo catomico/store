64ee35225cd427ff15e5d982de787446ac3dd654
user86 <user86marc@gmail.com>
2021-05-21 08:38:43 +0200

    started adding cart dropdown

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './button.scss';
 
-// a button and input can share the type of submit but here we need to also add the forms onSubmit, so start by passing in children and then destructure all otherProps
+// a button and input can share the type of submit but here we need to also add the form's onSubmit, so start by passing in children and then destructure all otherProps
 const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button type='button'
     className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
---
+++ src/components/cart-down/cart-down.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Button from '../button';
+
+import './cart-down.scss';
+
+const Cart = () => (
+  <div className='cart-down'>
+    <div className='cart-items' />
+    <Button>GO TO CHECKOUT</Button>
+  </div>
+)
+
+export default Cart;
