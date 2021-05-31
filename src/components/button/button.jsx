e787babf2a16468282852e128406bd34e92faafc
user86 <user86marc@gmail.com>
2021-05-31 08:27:32 +0200

    added buton to collection items

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import './button.scss';
+// this is a stateless functional componentor presentational compo.
+// accepts dynamically added style classes
 
 // a button and input can share the type of submit but here we need to also add the form's onSubmit, so start by passing in children and then destructure all otherProps
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button type='button'
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
+// In this case children => names that appear on the buttons where they are used.
+// other props => properties like class, or type. i.e, in signin form we add a prop of type = submit. but note , that here we add type = button to fix a glitch
+
+// These $ styles are in the button stylesheet
+
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button 
+    type='button'
+    className={`
+      ${inverted ? 'inverted' : ''}
+      ${isGoogleSignIn ? 'google-sign-in' : ''} 
+      custom-button
+    `} 
+
     {...otherProps}
   >
     {children}
--- src/components/collection-items/items.jsx
+++ src/components/collection-items/items.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import Button from '../button/button';
 
 import './items.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className='coll-item'> 
     <div
-    className='image'
-    style={{
-      backgroundImage:  `URL(${imageUrl})`
-    }}
+      className='image'
+      style={{
+        backgroundImage:  `URL(${imageUrl})`
+      }}
     />
     <div className='footer'>
       <span className='name'>{ name }</span>
       <span className='price'>{ price }</span>
     </div>
+    <Button inverted>Add to Cart</Button>
   </div>
 )
 
